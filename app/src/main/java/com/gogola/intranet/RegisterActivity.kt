391d7a23c3ec8094845215fbd1057c8bb7f50738
Levan Gogoladze <l_gogoladze1@cu.edu.ge>
2021-01-26 01:37:09 +0400

    Add functionality to save users data in the Firebase database.

--- app/src/main/java/com/gogola/intranet/RegisterActivity.kt
+++ app/src/main/java/com/gogola/intranet/RegisterActivity.kt
@@ -2,10 +2,10 @@ package com.gogola.intranet
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton
 import com.gogola.intranet.extinsions.validateEmail
 import com.gogola.intranet.extinsions.validateFirstName
@@ -14,6 +14,7 @@ import com.gogola.intranet.extinsions.validatePassword
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
@@ -23,12 +24,15 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var lastName: EditText
     private lateinit var email: EditText
     private lateinit var password: EditText
+    private lateinit var sidebarLoginIn: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide();
         setContentView(R.layout.activity_register)
         auth = Firebase.auth
+        val db = Firebase.firestore
+        sidebarLoginIn = findViewById(R.id.sidebarLoginIn)
 
         fun onLoginClick() {
             startActivity(Intent(this, LoginActivity::class.java))
@@ -76,7 +80,6 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         val haveAccountText = findViewById<TextView>(R.id.haveAccount);
-        val sidebarLoginIn = findViewById<ImageView>(R.id.sidebarLoginIn)
         val registerBtn = findViewById<CircularProgressButton>(R.id.cirRegisterButton)
 
         haveAccountText.setOnClickListener() {
@@ -91,10 +94,10 @@ class RegisterActivity : AppCompatActivity() {
             val progressBar: RelativeLayout = findViewById(R.id.registerProgressBar)
             val registerMainContent: ScrollView = findViewById(R.id.registerMainContent)
             val view: View = findViewById(R.id.registerView)
-            val sidebarLogin: ImageView = findViewById(R.id.sidebarLoginIn)
+
             if (validate()) {
                 progressBar.visibility = View.VISIBLE
-                sidebarLogin.visibility = View.GONE
+                sidebarLoginIn.visibility = View.GONE
                 view.visibility = View.GONE
                 registerMainContent.visibility = View.GONE
 
@@ -104,14 +107,36 @@ class RegisterActivity : AppCompatActivity() {
                         password.text.toString()
                     )
                         .addOnCompleteListener(this) { task ->
+
                             if (task.isSuccessful) {
-                                Toast.makeText(
-                                    baseContext, "Registration completed.",
-                                    Toast.LENGTH_SHORT
-                                ).show()
+                                val userId = auth.currentUser?.uid
+                                val userData = hashMapOf(
+                                    "UID" to userId.toString(),
+                                    "email" to email.text.toString(),
+                                    "firstName" to firstName.text.toString(),
+                                    "lastName" to lastName.text.toString()
+                                )
+                                db.collection("usersData")
+                                    .add(userData)
+                                    .addOnSuccessListener { documentReference ->
+                                        Toast.makeText(
+                                            baseContext, "Registration completed.",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                    .addOnFailureListener { e ->
+                                        progressBar.visibility = View.GONE
+                                        sidebarLoginIn.visibility = View.VISIBLE
+                                        view.visibility = View.VISIBLE
+                                        registerMainContent.visibility = View.VISIBLE
+                                        Toast.makeText(
+                                            baseContext, "Registration failed.",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
                             } else {
                                 progressBar.visibility = View.GONE
-                                sidebarLogin.visibility = View.VISIBLE
+                                sidebarLoginIn.visibility = View.VISIBLE
                                 view.visibility = View.VISIBLE
                                 registerMainContent.visibility = View.VISIBLE
                                 Toast.makeText(
